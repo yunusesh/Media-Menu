d728d9a197bc36fff5464dafc638bc3d23dab88a
Yunus Eshesh <yunuseshesh@gmail.com>
2025-10-13 03:15:39 -0400

    navigation between track/music/album

--- frontend/src/pages/music-pages/Track.js
+++ frontend/src/pages/music-pages/Track.js
@@ -1,10 +1,10 @@
 import "./Track.css"
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useQuery} from "react-query";
 
 export function Track(){
     const {id} = useParams();
-
+    const navigate = useNavigate();
     async function fetchTrack(){
         const response = await fetch(`http://localhost:8081/track/${id}`)
 
@@ -26,12 +26,24 @@ export function Track(){
     }
 
     return(
-        <div className = "track">
-            <h3>{data?.title}</h3>
-           <img className = "img"
-                src = {`https://coverartarchive.org/release-group/${data?.release.id}/front`}
-                alt = 'placeholder.jpg'
-                />
+        <div className = "track-page">
+            <img className = "img"
+                 src = {`https://coverartarchive.org/release-group/${data?.release.id}/front`}
+                 alt = 'placeholder.jpg'
+            />
+            <div className = "title-above-release">
+                <div className = "title-date-artist">
+                    <h1 className = "title">{data?.title}</h1>
+                    <h3 className = "date">{data?.["first-release-date"]?.substring(0,4)} • Track by </h3>
+                    <h2 className = "artist"
+                        onClick={() => navigate(`/music/artist/${data?.release["artist-credit"]?.[0]?.artist.id}`)}
+                    >{data?.release["artist-credit"]?.[0]?.name}</h2>
+                </div>
+            <h3 className = "release">Appears on</h3>
+                <h2 className = "release-title"
+                    onClick={() => navigate(`/music/album/${data.release.id}`)}
+                >{data?.release?.title}</h2>
+            </div>
         </div>
 
     )
